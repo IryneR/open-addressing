80ad20df3f48f9bfee54c763fd448eeae59b43f7
Iryna <irishka2991@gmail.com>
2020-06-11 17:04:30 +0300

    Rewrite test project with Double Hashing algorithm Fix hash function Rewrite Node class as private static inner class

--- src/main/java/org/test/OpenAddressing.java
+++ src/main/java/org/test/OpenAddressing.java
@@ -1,10 +1,14 @@
 package org.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 public class OpenAddressing {
     private static final int INIT_CAPACITY = 4;
 
-    private int n;           // number of key-value pairs
-    private int m;           // size of the  table
+    private int numberOfElements;
+    private int tableSize; // it needs to be a power of 2
     private Node[] nodes;
 
     public OpenAddressing() {
@@ -12,48 +16,79 @@ public class OpenAddressing {
     }
 
     public OpenAddressing(int capacity) {
-        m = capacity;
-        n = 0;
-        nodes = new Node[m];
+        if (capacity <= 0){
+            tableSize = INIT_CAPACITY;
+        }
+        else {
+            tableSize = nextPowerOf2(capacity);
+        }
+        numberOfElements = 0;
+        nodes = new Node[tableSize];
+    }
+
+    // compute power of two greater than or equal to n
+    private static int nextPowerOf2(int n)
+    {
+        // decrement n (to handle cases when n itself is a power of 2)
+        n = n - 1;
+        // do till only one bit is left
+        while ((n & n - 1) != 0) {
+            n = n & n - 1;	// unset rightmost bit
+        }
+
+        // n is now a power of two (less than n)
+        // return next power of 2
+        return n << 1;
     }
 
     public int size() {
-        return n;
+        return numberOfElements;
     }
 
-    private int hash(int key) {
-        return Math.abs(key) % m;
+    public int hashFunctionFirst(int key) {
+        return (key & 0x7fffffff) % tableSize;
+    }
+
+    public int hashFunctionSecond(int key) {
+        return 5 - ((key & 0x7fffffff) % 5);
     }
 
     private void resize(int capacity) {
         OpenAddressing temp = new OpenAddressing(capacity);
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < tableSize; i++) {
             if (nodes[i] != null) {
                 temp.put(nodes[i].getKey(), nodes[i].getValue());
             }
         }
         nodes = temp.nodes;
-        m = temp.m;
+        tableSize = temp.tableSize;
     }
 
     public void put(int key, long val) {
 
         // double table size if 50% full
-        if (n >= m / 2) resize(2 * m);
+        if (numberOfElements >= tableSize / 2) resize(2 * tableSize);
 
-        int i;
-        for (i = hash(key); nodes[i] != null; i = (i + 1) % m) {
+        int i, j;
+        int hashValue = hashFunctionFirst(key); // hash the key
+        int stepSize = hashFunctionSecond(key); // get step size
+
+        for (i = hashValue, j=0; nodes[i] != null && j>=0; i = (hashValue + j * stepSize) % tableSize, j++) {
             if (nodes[i].getKey()==(key)) {
                 nodes[i].setValue(val);
                 return;
             }
         }
         nodes[i] = new Node(key, val);
-        n++;
+        numberOfElements++;
     }
 
-    public Node get(int key) {
-        for (int i = hash(key); nodes[i] != null; i = (i + 1) % m){
+     public Node get(int key) {
+        int i, j;
+        int hashValue = hashFunctionFirst(key); // hash the key
+        int stepSize = hashFunctionSecond(key); // get step size
+
+        for (i = hashValue, j=0; nodes[i] != null && j>=0; i = (hashValue + j * stepSize) % tableSize, j++) {
             if (nodes[i].getKey() == key)
                 return nodes[i];
         }
@@ -61,13 +96,26 @@ public class OpenAddressing {
     }
 
     public int getCapacity() {
-        return m;
+        return tableSize;
     }
 
     public int getIndexOfKey(int key) {
-                for (int i = hash(key); nodes[i] != null; i = (i + 1) % m)
+        int i, j;
+        int hashValue = hashFunctionFirst(key); // hash the key
+        int stepSize = hashFunctionSecond(key); // get step size
+
+        for (i = hashValue, j=0; nodes[i] != null && j>=0; i = (hashValue + j * stepSize) % tableSize, j++)
             if (nodes[i].getKey() == key)
                 return i;
         return -1;
     }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Node {
+        private int key;
+        private long value;
+    }
+
 }
--- src/test/java/org/test/AppTest.java
+++ src/test/java/org/test/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(1234567890, 1234567891234567891L);
-        Node value = openAddressingHashTable.get(1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(1234567890);
         assertEquals(1234567891234567891L, value.getValue());
 
     }
@@ -25,7 +25,7 @@ public class AppTest
 
         openAddressingHashTable.put(1234567890, 1234567891234567891L);
         openAddressingHashTable.put(1234567890, 1000000000000000000L);
-        Node value = openAddressingHashTable.get(1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(1234567890);
         assertEquals(1000000000000000000L,value.getValue());
     }
 
@@ -35,7 +35,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(-1234567890, 1234567891234567891L);
-        Node value = openAddressingHashTable.get(-1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(-1234567890);
         assertEquals(1234567891234567891L, value.getValue());
     }
 
@@ -45,7 +45,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(1234567890, -1234567891234567891L);
-        Node value = openAddressingHashTable.get(1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(1234567890);
         assertEquals(-1234567891234567891L,value.getValue());
     }
 
@@ -55,7 +55,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(-1234567890, -1234567891234567891L);
-        Node value = openAddressingHashTable.get(-1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(-1234567890);
         assertEquals(-1234567891234567891L, value.getValue());
     }
 
@@ -65,7 +65,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(0, 1234567891234567891L);
-        Node value = openAddressingHashTable.get(0);
+        OpenAddressing.Node value = openAddressingHashTable.get(0);
         assertEquals(1234567891234567891L, value.getValue());
     }
 
@@ -75,7 +75,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(0, -1234567891234567891L);
-        Node value = openAddressingHashTable.get(0);
+        OpenAddressing.Node value = openAddressingHashTable.get(0);
         assertEquals(-1234567891234567891L,value.getValue());
     }
 
@@ -85,7 +85,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(0, 0L);
-        Node value = openAddressingHashTable.get(0);
+        OpenAddressing.Node value = openAddressingHashTable.get(0);
         assertEquals(0L,value.getValue());
     }
 
@@ -95,7 +95,7 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(1234567890, 0L);
-        Node value = openAddressingHashTable.get(1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(1234567890);
         assertEquals(0L,value.getValue());
     }
 
@@ -105,15 +105,56 @@ public class AppTest
         OpenAddressing openAddressingHashTable = new OpenAddressing();
 
         openAddressingHashTable.put(-1234567890, 0L);
-        Node value = openAddressingHashTable.get(-1234567890);
+        OpenAddressing.Node value = openAddressingHashTable.get(-1234567890);
         assertEquals(0L,value.getValue());
+    }
+
+    @Test
+    public void putGetSimpleMinIntKeyPositiveValueTest()
+    {
+        OpenAddressing openAddressingHashTable = new OpenAddressing();
+
+        openAddressingHashTable.put(Integer.MIN_VALUE, 1234567890L);
+        OpenAddressing.Node value = openAddressingHashTable.get(Integer.MIN_VALUE);
+        assertEquals(1234567890L,value.getValue());
+    }
+
+    @Test
+    public void putGetSimpleMaxIntKeyPositiveValueTest()
+    {
+        OpenAddressing openAddressingHashTable = new OpenAddressing();
+
+        openAddressingHashTable.put(Integer.MAX_VALUE, 1234567890L);
+        OpenAddressing.Node value = openAddressingHashTable.get(Integer.MAX_VALUE);
+        assertEquals(1234567890L,value.getValue());
+    }
+
+    @Test
+    public void checkPowerOfTwoPositiveTest()
+    {
+        OpenAddressing openAddressingHashTable = new OpenAddressing(5);
+
+        openAddressingHashTable.put(Integer.MAX_VALUE, 1234567890L);
+        OpenAddressing.Node value = openAddressingHashTable.get(Integer.MAX_VALUE);
+        assertEquals(1234567890L,value.getValue());
+        assertEquals(8, openAddressingHashTable.getCapacity());
+    }
+
+    @Test
+    public void checkPowerOfTwoNegativeTest()
+    {
+        OpenAddressing openAddressingHashTable = new OpenAddressing(-2);
+
+        openAddressingHashTable.put(Integer.MAX_VALUE, 1234567890L);
+        OpenAddressing.Node value = openAddressingHashTable.get(Integer.MAX_VALUE);
+        assertEquals(1234567890L,value.getValue());
+        assertEquals(4, openAddressingHashTable.getCapacity());
     }
 
 
     @Test
     public void putGetWithCapacityFourTest()
     {
-
         OpenAddressing openAddressingHashTable = new OpenAddressing(4);
 
         openAddressingHashTable.put(-1234567890, 0L);
@@ -137,59 +178,91 @@ public class AppTest
         assertEquals(8, capacity);
     }
 
-    @Test
-    public void putGetWithCapacityFiveTest()
-    {
-
-        OpenAddressing openAddressingHashTable = new OpenAddressing(5);
-
-        openAddressingHashTable.put(-1234567890, 0L);
-        openAddressingHashTable.put(1234567890, 12345L);
-
-        int capacity = openAddressingHashTable.getCapacity();
-        assertEquals(5,capacity);
-    }
 
     @Test
-    public void putGetWithCapacityTenTest()
+    public void putFirstProbingTest()
     {
+        OpenAddressing openAddressingHashTable = new OpenAddressing(8);
 
-        OpenAddressing openAddressingHashTable = new OpenAddressing(5);
+        openAddressingHashTable.put(28, 1500L);
+        int bucketNumber= (28 % 8);
 
-        openAddressingHashTable.put(-1234567890, 0L);
-        openAddressingHashTable.put(1234567890, 12345L);
-        openAddressingHashTable.put(1000000000, -12345L);
-
-        int capacity = openAddressingHashTable.getCapacity();
-        assertEquals(10,capacity);
+        assertEquals(bucketNumber,openAddressingHashTable.getIndexOfKey(28));
     }
 
     @Test
     public void putKeyToTheNextNodeTest()
     {
         // Try to put 2 different keys with identical hash numbers and check if second key
-        //is on the position  (hash number +1)
+        //is on the position  (hash1 +1*hash2)%m
         OpenAddressing openAddressingHashTable = new OpenAddressing(8);
 
-        openAddressingHashTable.put(12, 1000L);
-        openAddressingHashTable.put(28, 1500L);
-        int bucketNumber= (28 % 8)+1;
+        openAddressingHashTable.put(12, 1000L); //node 4
+        openAddressingHashTable.put(28, 1500L);// node 4 -> node 6
+        int bucketNumber= (28 % 8)+1*(5-(28 % 5)) % 8;
 
+        OpenAddressing.Node value1 = openAddressingHashTable.get(12);
+        OpenAddressing.Node value2 = openAddressingHashTable.get(28);
+
+        assertEquals(1000L, value1.getValue());
+        assertEquals(1500L, value2.getValue());
         assertEquals(bucketNumber,openAddressingHashTable.getIndexOfKey(28));
+    }
+
+    @Test
+    public void putKeySecondProbingTest()
+    {
+        // Try to put 2 different keys with identical hash numbers and check if second key
+        //is on the position  (hash1 +2*hash2)%m
+        OpenAddressing openAddressingHashTable = new OpenAddressing(16);
+
+        openAddressingHashTable.put(14, 1000L); // node 14
+        openAddressingHashTable.put(3, 1500L); //node 3
+        openAddressingHashTable.put(30, -1500L); //node 14 -> node 8
+
+        int bucketNumber = ((30 % 16)+2*(5-(30 % 5))) % 16; // node 8
+
+        OpenAddressing.Node value = openAddressingHashTable.get(30);
+
+        assertEquals(-1500L, value.getValue());
+        assertEquals(bucketNumber,openAddressingHashTable.getIndexOfKey(30));
+
     }
 
     @Test
     public void putKeyToTheFirstFreeNodeTest()
     {
-        // Try to put 2 different keys with identical hash numbers and check if second key
-        //is in the first node, if there are no free nodes till the end of the hashtable
         OpenAddressing openAddressingHashTable = new OpenAddressing(8);
 
-        openAddressingHashTable.put(14, 1000L); //node 6
-        openAddressingHashTable.put(15, 1000L); //node 7
-        openAddressingHashTable.put(22, 1500L);
+        openAddressingHashTable.put(6, 1000L); //node 6
+        openAddressingHashTable.put(3, 1500L); //node 3
+        openAddressingHashTable.put(30, -1500L); //node 6 - > node 0
 
-        assertEquals(0,openAddressingHashTable.getIndexOfKey(22));
+        int bucketNumber = ((30 % 8)+2*(5-(30 % 5))) % 8; // node 0
+
+        OpenAddressing.Node value = openAddressingHashTable.get(30);
+
+        assertEquals(-1500L, value.getValue());
+        assertEquals(0,openAddressingHashTable.getIndexOfKey(30));
+    }
+
+    @Test
+    public void putKeySecondProbingRewriteValueTest()
+    {
+        OpenAddressing openAddressingHashTable = new OpenAddressing(8);
+
+        openAddressingHashTable.put(6, 1000L); //node 6
+        openAddressingHashTable.put(3, 1500L); //node 3
+        openAddressingHashTable.put(30, -1500L); //node 6 - > node 0
+
+        openAddressingHashTable.put(30, 1500L); //node 6 - > node 0
+
+        int bucketNumber = ((30 % 8)+2*(5-(30 % 5))) % 8; // node 0
+
+        OpenAddressing.Node value = openAddressingHashTable.get(30);
+
+        assertEquals(1500L, value.getValue());
+        assertEquals(0, openAddressingHashTable.getIndexOfKey(30));
     }
 
     @Test
@@ -205,63 +278,39 @@ public class AppTest
     }
 
     @Test
-    public void getExistedValueFromTheClusterTest()
+    public void getExistedValueTest()
+    {
+        OpenAddressing openAddressingHashTable = new OpenAddressing(32);
+
+        openAddressingHashTable.put(53, 1000L); // node 21
+        openAddressingHashTable.put(25, 1000L); //node 25
+        openAddressingHashTable.put(29, 0L); //node 29
+        openAddressingHashTable.put(1, 0L); //node 1
+
+        openAddressingHashTable.put(21, -1500L); //node 21 -> node 5
+
+        int bucketNumber = ((21 % 32)+4*(5-(21 % 5))) % 32; //node 5
+
+        OpenAddressing.Node value = openAddressingHashTable.get(21);
+        assertEquals(-1500L, value.getValue());
+        assertEquals(bucketNumber, openAddressingHashTable.getIndexOfKey(21));
+    }
+
+    @Test
+    public void getNonExistedValueTest()
     {
         OpenAddressing openAddressingHashTable = new OpenAddressing(23);
 
-        openAddressingHashTable.put(28, 1000L); // node 5
-        openAddressingHashTable.put(74, 1000L); //node 5 -> node 6
-        openAddressingHashTable.put(51, 0L); //node 5 -> node 7
-        openAddressingHashTable.put(31, -1500L); //node 8
 
-        Node value = openAddressingHashTable.get(51);
-        assertEquals(0L, value.getValue());
-        assertEquals(7, openAddressingHashTable.getIndexOfKey(51));
-    }
+        openAddressingHashTable.put(53, 1000L); // node 21
+        openAddressingHashTable.put(25, 1000L); //node 25
+        openAddressingHashTable.put(29, 0L); //node 29
+        openAddressingHashTable.put(1, 0L); //node 1
 
-    @Test
-    public void getNonExistedValueFromTheClusterTest()
-    {
-        OpenAddressing openAddressingHashTable = new OpenAddressing(23);
-
-        openAddressingHashTable.put(28, 1000L);
-        openAddressingHashTable.put(74, 1000L);
-        openAddressingHashTable.put(51, 0L);
-        openAddressingHashTable.put(31, -1500L);
-
-        Node value = openAddressingHashTable.get(5);
+        OpenAddressing.Node value = openAddressingHashTable.get(21);
         assertNull(value);
-        assertEquals(-1, openAddressingHashTable.getIndexOfKey(5));
+        assertEquals(-1, openAddressingHashTable.getIndexOfKey(21));
     }
 
-    @Test
-    public void getExistedValueShiftedToBeginTest()
-    {
-        OpenAddressing openAddressingHashTable = new OpenAddressing(8);
-
-        openAddressingHashTable.put(14, 1000L); //node 6
-        openAddressingHashTable.put(15, 1000L); //node 7
-        openAddressingHashTable.put(0, 1500L); // node 0
-        openAddressingHashTable.put(22, -3000L); // node 6 -> node 1
-
-        Node value = openAddressingHashTable.get(22);
-        assertEquals(-3000L, value.getValue());
-        assertEquals(1, openAddressingHashTable.getIndexOfKey(22));
-    }
-
-    @Test
-    public void getNonExistedValueShiftedToBeginTest()
-    {
-        OpenAddressing openAddressingHashTable = new OpenAddressing(8);
-
-        openAddressingHashTable.put(14, 1000L); //node 6
-        openAddressingHashTable.put(15, 1000L); //node 7
-        openAddressingHashTable.put(0, 1500L); // node 0
-        openAddressingHashTable.put(22, -3000L); // node 6 -> node 1
-
-        Node value = openAddressingHashTable.get(30);
-        assertNull(value);
-        assertEquals(-1, openAddressingHashTable.getIndexOfKey(30));
-    }
 
 }
